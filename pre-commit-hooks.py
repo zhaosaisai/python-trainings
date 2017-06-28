745f14e33c10124eb8422d4caf49efb3fc9d21a2
zhaosaisai <zhaosaisai@weidian.com>
2017-06-29 00:13:25 +0800

    complete auto hooks

--- pre-commit-hooks.py
+++ pre-commit-hooks.py
@@ -6,7 +6,7 @@
 import os
 import yaml
 
-SOURCE_DIR = os.getcwd()+'/source.1'
+SOURCE_DIR = os.getcwd()+'/source'
 TITLE_CONFIG = yaml.load(open('./title.yml'))['titles']
 
 for file in os.listdir(SOURCE_DIR):
@@ -19,16 +19,10 @@ for file in os.listdir(SOURCE_DIR):
     except:
         continue
 
+print 'auto change filename successfully'
+
 # 命名结束之后中git命令
 try:
-    add_status = os.system('git add .')
-    if add_status == 0:
-        #删除软链
-        os.system('rm -f %s/.git/hooks/pre-commit' % SOURCE_DIR)
-    else:
-        exit('git add命令失败')
-    print os.popen('git commit -m "auto update file name by hooks"')
-    #重新创建软链
-    os.system('ln -s %s %s/.git/hooks/pre-commit' % (__file__, SOURCE_DIR))
+    os.system('git add .')
 except:
     exit('执行自动化git命令失败')
